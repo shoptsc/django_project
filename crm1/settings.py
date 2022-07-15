413d12ac23449afd2bdc2e7233a12a2fcf750f64
User <user@Users-iMac.local>
2022-07-15 19:05:01 -0400

    edited

--- crm1/settings.py
+++ crm1/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hamed-crm1.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['hamed-crm1.herokuapp.com', '127.0.0.1']
 
 # ALLOWED_HOSTS = []
 
